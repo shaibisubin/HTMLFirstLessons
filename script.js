34835365bc8ba6304f0e07c452375c768d7a43f2
prabin <prabintp@gmail.com>
2020-04-19 15:34:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('hi its working');
+document.getElementById('flexheading').innerHTML= 'New Flex heading';
+var a = 10;
+var b = 20;
+
+// to calculate sum
+function sum(x,y) {
+    var c = 2;
+    return x + y +c;
+}
+
+console.log(c);
+document.getElementById('flexheading').innerHTML= 'New Flex heading' + sum(10, 50);
